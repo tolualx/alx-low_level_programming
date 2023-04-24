7ea0ee04c6d546e55b23aa214b4d80941ae490f5
tolualx <ojumahtolu@gmail.com>
2023-04-24 12:57:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdo.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
