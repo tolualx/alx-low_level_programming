d72f17a0e58b9c86379ff347589ab1e8d2281a7c
tolualx <ojumahtolu@gmail.com>
2023-04-24 13:18:13 -0700

    prnt alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,13 @@ int main(void)
 {
 	char ch;
 
-	for (ch - 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch - 'A'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'z'; ch++)
 		putchar(ch);
 
 	putchar('\n');
 
 	return (0);
 }
-
