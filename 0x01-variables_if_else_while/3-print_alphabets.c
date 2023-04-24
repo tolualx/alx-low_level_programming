9de9823e8600960067fe284303301b4a2e0ff485
tolualx <ojumahtolu@gmail.com>
2023-04-24 13:12:42 -0700

    prnt alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in lower and upper case
+ * Return: always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch - 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch - 'A'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
+
