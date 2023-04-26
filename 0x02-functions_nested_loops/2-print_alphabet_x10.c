9333f2b1d83bb66b82fa5bc94a52070ad7196afb
tolualx <ojumahtolu@gmail.com>
2023-04-26 15:23:52 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet x10
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int 1 = 0;
+
+	while (1 <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		1+;
+	}
+}
