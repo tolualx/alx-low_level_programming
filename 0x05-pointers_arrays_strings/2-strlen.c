4cc704693b8f5106a159dd1ba0d665273c771bcb
tolualx <ojumahtolu@gmail.com>
2023-05-02 05:38:47 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,18 @@
 
 /**
  * _strlen -function that returns the length of a string
- * @s: String pointer to the string whose length is to be found
- * Return: returns the length of the string
+ * @s: String pointer
+ * Return: length of the string
  */
 
 int _strlen(char *s)
 {
-	int p = 0;
+	int len = 0;
 	/ increment up to when the last character is NULL, \ 0 /
-		while (*(s + p) != 0)
+		while (*s != '\0')
 		{
-			p++;
+			len++;
+			s++;
 		}
-	return (p);
+	return (len);
 }
